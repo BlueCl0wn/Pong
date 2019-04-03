6466e09874d3b7df14cc7f853e2d4918cf50d3fb
BlueCl0wn <45670653+BlueCl0wn@users.noreply.github.com>
2019-04-03 21:33:13 +0200

    Gleiche Version wie zuvor nur kommentiert.

--- Pong.py
+++ Pong.py
@@ -2,13 +2,14 @@ import pygame
 from random import randint
 import time
 
+# Spielefenster wird erstellt und weitere Nebeneinstellungen
 pygame.init()
-
 sizex = 1200
 sizey = 675
 win = pygame.display.set_mode((sizex, sizey))
 pygame.display.set_caption("Pong")
 
+# Klasse Ball mit der der Ball definiert wird
 class ball():
     def __init__(self, color=(255, 0 ,0)):
         self.velx = randint(-6,6)
@@ -28,56 +29,68 @@ class ball():
     #     if count == 150:
     #         self.move = 1
 
+# Klasse Player mit der die beiden Spieler definiert werden.
 class player():
     def __init__(self, x, y, vel, color):
         self.x = x
         self.y = y
         self.vel = vel
         self.color = color
+
+    # Funktion zum bewegen des linken Spielers
     def moveleft(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
             self.y -= self.vel
+            # erzeugt Spieleborder
             if self.y < 0:
                 self.y = 0
         if keys[pygame.K_s]:
             self.y += self.vel
+            # erzeugt Spieleborder
             if self.y > 575:
                 self.y = 575
+    # Funktion zum bewegen des rechten Spielers.
     def moveright(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
             self.y -= self.vel
+            # erzeugt Spieleborder
             if self.y < 0:
                 self.y = 0
         if keys[pygame.K_DOWN]:
             self.y += self.vel
+            # erzeugt Spieleborder
             if self.y > 575:
                 self.y = 575
     def draw(self):
         pygame.draw.rect(win, self.color, (self.x, self.y, 20, 100))
 
 
-
+# Instanzen des Balles und der Spieler werden erstellt.
 pl_left = player(x=20, y=300, vel=5, color=(0, 255, 0))
 pl_right = player(x=1160, y=300, vel=5, color=(0, 0, 255))
-
 ball = ball()
 
+# Main-Loop
 run = 1
 while run:
     pygame.time.delay(30)
 
+    # Es wird überprüft ob das Spiel geschlossen werden soll.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = 0
 
+    # Füllen des Spielefensters.
     win.fill((255, 255, 255))
 
+    # Funktionen zum Bewegen der Instanzen.
     pl_left.moveleft()
     pl_right.moveright()
     ball.moveball()
 
+    # Instanzen werden gezeichnet.
     pl_left.draw()
     pl_right.draw()
     ball.draw()
